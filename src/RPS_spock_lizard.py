f96fe01f69836de48ebb8e1f35fbf5fb9aeb58b8
XianGomez <xiiangomezz@gmail.com>
2026-02-12 09:23:28 +0100

    feat(RPS_spock_lizard): Añadir clase Game

--- src/RPS_spock_lizard.py
+++ src/RPS_spock_lizard.py
@@ -11,3 +11,11 @@ class GameResult:
     TIE = 1
     VICTORY = 2 
     
+class Game:
+    victories = {
+        GameAction.ROCK: [GameAction.SCISSORS, GameAction.LIZARD],
+        GameAction.PAPER: [GameAction.ROCK, GameAction.SPOCK],
+        GameAction.SCISSORS: [GameAction.PAPER, GameAction.LIZARD],
+        GameAction.LIZARD: [GameAction.SPOCK, GameAction.PAPER],
+        GameAction.SPOCK: [GameAction.SCISSORS, GameAction.ROCK],
+    }
