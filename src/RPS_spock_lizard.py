4d6ff88899abd130a7a09c6f642eca65f4b08f19
XianGomez <xiiangomezz@gmail.com>
2026-02-13 13:43:23 +0100

    feat(RPS_spock_lizard): Añadir input usuario para game

--- src/RPS_spock_lizard.py
+++ src/RPS_spock_lizard.py
@@ -1,10 +1,14 @@
+import random
+
 
 class GameAction:
-    ROCK = 0
-    PAPER = 1
-    SCISSORS = 2
-    SPOCK = 3
-    LIZARD = 4
+    ROCK = "ROCK"
+    PAPER = "PAPER"
+    SCISSORS = "SCISSORS"
+    SPOCK = "SPOCK"
+    LIZARD = "LIZARD"
+
+    valid_actions = [ROCK, PAPER, SCISSORS, SPOCK, LIZARD]
 
 class GameResult:
     DEFEAT = 0
@@ -12,6 +16,8 @@ class GameResult:
     VICTORY = 2 
     
 class Game:
+    
+
     victories = {
         GameAction.ROCK: [GameAction.SCISSORS, GameAction.LIZARD],
         GameAction.PAPER: [GameAction.ROCK, GameAction.SPOCK],
@@ -32,10 +38,34 @@ class Game:
 
         print(f"Derrota: {computer_action} vence a {user_action}")
         return GameResult.DEFEAT
+
+    def user_action(self):
+        
+        valid_input = False
+        while valid_input == False:
+            user_action = input("Selecciona una de las siguientes opciones:'ROCK', 'PAPER', 'SCISSORS', 'SPOCK', 'LIZARD': ").upper()
+            print(f"input: {user_action}")
+            if user_action in GameAction.valid_actions:
+                print("GOD")
+                valid_input = True
+                return user_action
+            else:
+                print("Acción denegada, inserte una opción válida")
+
+
+    def computer_action():
+        computer_action = random.choice(GameAction)
+        print(computer_action)
+        return computer_action
     
-    def play():
+    def replay():
         pass
 
+    def play(self):
+        Game.user_action(self)
+        
+
 if __name__ == "__main__":
+    
     game = Game()
     game.play()
